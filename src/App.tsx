aad65cc0aeb0e3bebf019dc32e488b8c6750a5f4
N.I <iworksWeb@gmail.com>
2025-02-13 23:55:53 +0900

    add useState()

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState<number>(0);
   return (
     <div id="root">
-      <h1>Hello World</h1>
+      <h1>useState</h1>
+      <small>setCountが呼ばれるたびに再レンダリングが発生します</small>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
     </div>
   );
 }
