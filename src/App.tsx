ec1b32fdca4785b81d82d36452d7f5b67b6d52e0
N.I <iworksWeb@gmail.com>
2025-02-15 01:36:30 +0900

    refact useCallback component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import "./App.css";
-import SomeChild from "./components/SomeChild";
 import UseContext from "./components/UseContext";
 import UseEffect from "./components/UseEffect";
 import UseRef from "./components/UseRef";
 import UseState from "./components/useState";
-import { useState, useCallback } from "react";
 import UseReducer from "./components/useReducer";
 import UseMemo from "./components/UseMemo";
+import UseCallback from "./components/UseCallback";
 
 function App() {
-  const [counter, setCounter] = useState<number>(0);
-  const showCount = useCallback(() => {
-    let i = 0;
-    while (i < 1000000) i++;
-    alert("重い処理です。");
-  }, [counter]);
-
   return (
     <div id="root">
       <UseState />
@@ -24,17 +16,7 @@ function App() {
       <UseRef />
       <UseReducer />
       <UseMemo />
-
-      <div className="mt-50">
-        <h1>useCallback</h1>
-        <small>
-          <ul>
-            <li>メモ化(関数をブラウザのキャッシュに保存)</li>
-            <li>関数をメモ化することで、関数の再生成を避けることができる</li>
-          </ul>
-        </small>
-        <SomeChild showCount={showCount} />
-      </div>
+      <UseCallback />
     </div>
   );
 }
